c774ea6f56b63c7399b692206eed24af18dbbaae
Amirhossein Akbari <amirhossein.akbari@dci-student.org>
2025-08-25 21:40:23 +0200

    Update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // 👈 مهم! برای اینکه asset ها همیشه از root لود بشن
+});
