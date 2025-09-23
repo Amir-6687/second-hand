1f9ba64f8aa349fd676b1e2348d27902265a4035
Amirhossein Akbari <amirhossein.akbari@dci-student.org>
2025-09-23 22:12:06 +0200

    Change mobile hamburger menu close button (X) color to white

--- src/App.jsx
+++ src/App.jsx
@@ -472,6 +472,7 @@ function Navigation() {
                   strokeLinejoin="round"
                   strokeWidth={2}
                   d="M6 18L18 6M6 6l12 12"
+                  style={{ stroke: "white" }}
                 />
               ) : (
                 <>
