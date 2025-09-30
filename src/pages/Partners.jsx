39f1327965aee9b5bd91d70138df598d53123549
Amirhossein Akbari <amirhossein.akbari@dci-student.org>
2025-09-30 16:40:13 +0200

    Partners: lighten search/filter section background to white

--- src/pages/Partners.jsx
+++ src/pages/Partners.jsx
@@ -115,7 +115,7 @@ export default function Partners() {
         </section>
 
         {/* Filters Section */}
-        <section className="bg-gray-50 py-8">
+        <section className="bg-white py-8">
           <div className="container mx-auto px-4">
             <div className="max-w-4xl mx-auto">
               <form onSubmit={handleSearch} className="mb-6">
