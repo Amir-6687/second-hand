a1367671cdbd56852421e2b4e3ffdf15b50177a5
Amir Akbari <amir.hossein.akbari59@gmail.com>
2026-02-18 21:10:05 +0100

    update

--- backend/index.js
+++ backend/index.js
@@ -36,13 +36,6 @@ app.use(
   }),
 );
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  }),
-);
-
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
